59ef976f38d1eded268428088e876a75a613161e
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-01-23 10:12:29 +0100

    Add files via upload

---
+++ company_DB.sql
@@ -0,0 +1,15 @@
+CREATE TABLE company (
+	id INT PRIMARY KEY,
+	company_name VARCHAR (120)
+
+	
+);
+INSERT INTO company (id, company_name) VALUES (1, 'Lenovo');
+INSERT INTO company (id, company_name) VALUES (2, 'Apple');
+INSERT INTO company (id, company_name) VALUES (3, 'Samsung');
+INSERT INTO company (id, company_name) VALUES (4, 'Huawei');
+INSERT INTO company (id, company_name) VALUES (5, 'Fairphone');
+
+
+
+
