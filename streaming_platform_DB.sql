26a2eb42067ebcd28116e158a48d9f95cfb2b063
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-01-23 15:45:37 +0100

    Add files via upload

---
+++ streaming_platform_DB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE streaming_platform (
+	id INT PRIMARY KEY,
+	platform_name VARCHAR(120)
+
+);
+INSERT INTO streaming_platform (id, platform_name) VALUES (1, 'Netflix');
+INSERT INTO streaming_platform (id, platform_name) VALUES (2, 'HBO');
+INSERT INTO streaming_platform (id, platform_name) VALUES (3, 'Hulu');
+INSERT INTO streaming_platform (id, platform_name) VALUES (4, 'Mubi');
+INSERT INTO streaming_platform (id, platform_name) VALUES (5, 'Apple TV');
