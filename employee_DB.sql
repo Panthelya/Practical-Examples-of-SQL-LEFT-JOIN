8e9efb31a10ac5b18ec6a2d794263c3952d4c7f7
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-01-23 10:39:33 +0100

    Add files via upload

---
+++ employee_DB.sql
@@ -0,0 +1,17 @@
+CREATE TABLE employee (
+	id INT PRIMARY KEY,
+	first_name VARCHAR (120),
+	last_name VARCHAR (120),
+	department_id INT
+	
+);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (1, 'Bob', 'Estevez', 3);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (2, 'Francesca', 'Gotze', 2);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (3, 'Frank', 'Gordon', 2);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (4, 'Milicent', 'Johnson', 3);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (5, 'Hans', 'Handke', NULL);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (6, 'Katie', 'Keaton', 1);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (7, 'Luca', 'Di Francesco', NULL);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (8, 'Zoe', 'Jong', 1);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (9, 'Patrick', 'Rose', 2);
+INSERT INTO employee (id, first_name, last_name, department_id) VALUES (10, 'Billie', 'Thompson', NULL);
