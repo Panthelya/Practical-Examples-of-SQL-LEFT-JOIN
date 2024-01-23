65baef16f8f0272fbf995e414712951466ce3cc1
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-01-23 15:10:55 +0100

    Add files via upload

---
+++ customer_DB.sql
@@ -0,0 +1,16 @@
+CREATE TABLE customer (
+	id INT PRIMARY KEY,
+	first_name VARCHAR (120),
+	last_name VARCHAR (120)	
+	
+);
+INSERT INTO customer (id, first_name, last_name) VALUES (1, 'Florentinus', 'Glöckner');
+INSERT INTO customer (id, first_name, last_name) VALUES (2, 'Eman', 'Adcock');
+INSERT INTO customer (id, first_name, last_name) VALUES (3, 'Erik', 'Nyman');
+INSERT INTO customer (id, first_name, last_name) VALUES (4, 'Leeba', 'Kubo');
+INSERT INTO customer (id, first_name, last_name) VALUES (5, 'Lias', 'Vámos');
+INSERT INTO customer (id, first_name, last_name) VALUES (6, 'Lavanya', 'Nikolaev');
+INSERT INTO customer (id, first_name, last_name) VALUES (7, 'Rishi', 'Petit');
+INSERT INTO customer (id, first_name, last_name) VALUES (8, 'Christie', 'Fodor');
+INSERT INTO customer (id, first_name, last_name) VALUES (9, 'Andris', 'Lončar');
+INSERT INTO customer (id, first_name, last_name) VALUES (10, 'Juliana', 'Harlan');
