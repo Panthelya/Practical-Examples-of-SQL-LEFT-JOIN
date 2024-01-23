b28e3ea9d3bff787dd886ded2066bf16355e7d16
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-01-23 17:19:04 +0100

    Add files via upload

---
+++ translator_DB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE translator (
+	id INT PRIMARY KEY,
+	first_name VARCHAR(120),
+	last_name VARCHAR(120)
+
+);
+INSERT INTO translator (id, first_name, last_name) VALUES (1, 'Jennifer', 'Croft');
+INSERT INTO translator (id, first_name, last_name) VALUES (2, 'Peter', 'Constantine');
+INSERT INTO translator (id, first_name, last_name) VALUES (3, 'Ewald', 'Osers');
