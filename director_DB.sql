f5e7d78dd0b2868629c0a0a8c70080fdb318bcbe
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-01-23 15:39:29 +0100

    Add files via upload

---
+++ director_DB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE director (
+	id INT PRIMARY KEY,
+	first_name VARCHAR(120),
+	last_name VARCHAR(120)
+
+);
+INSERT INTO director (id, first_name, last_name) VALUES (1, 'Stanley', 'Kubrick');
+INSERT INTO director (id, first_name, last_name) VALUES (2, 'Céline', 'Sciamma');
+INSERT INTO director (id, first_name, last_name) VALUES (3, 'Woody', 'Allen');
+INSERT INTO director (id, first_name, last_name) VALUES (4, 'Lynne', 'Ramsay');
+INSERT INTO director (id, first_name, last_name) VALUES (5, 'Krzysztof', 'Kieślowski');
