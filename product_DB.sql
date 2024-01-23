ada2f44acc59826f33a30c44c630fc5a46eb9005
Panthelya <116551755+Panthelya@users.noreply.github.com>
2024-01-23 10:18:29 +0100

    Add files via upload

---
+++ product_DB.sql
@@ -0,0 +1,15 @@
+CREATE TABLE product (
+	id INT PRIMARY KEY,
+	product_name VARCHAR (120),
+	company_id INT
+
+	
+);
+INSERT INTO product (id, product_name, company_id) VALUES (1, 'Fairphone 4', 5);
+INSERT INTO product (id, product_name, company_id) VALUES (2, 'Galaxy S24 Ultra', 3);
+INSERT INTO product (id, product_name, company_id) VALUES (3, 'Galaxy Z Flip5', 3);
+INSERT INTO product (id, product_name, company_id) VALUES (4, 'iPhone 15 Pro', 2);
+INSERT INTO product (id, product_name, company_id) VALUES (5, 'Fairbuds XL', 5);
+INSERT INTO product (id, product_name, company_id) VALUES (6, 'MacBook Pro 16'' M3 Pro', 2);
+INSERT INTO product (id, product_name, company_id) VALUES (7, 'iPad Air 10.9'' M1', 2);
+INSERT INTO product (id, product_name, company_id) VALUES (8, 'Galaxy Tab S9 FE+', 3);
